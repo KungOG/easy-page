ae002a6b5fc6e6f3a9052af740d19ec8d1f5b63b
Oscar Gustafsson <oscar-gustafsson@live.com>
2020-10-26 00:49:11 +0100

    Cleaned button code abit

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,19 +3,17 @@ import StyledButton from "./style";
 
 const Button = ({ handleClick, isActive }) => {
   return (
-    <>
-      <StyledButton
-        active={isActive}
-        className={`${isActive ? "-active" : "-inactive"}`}
-        onClick={handleClick}
-      >
-        {!isActive ? (
-          <span data-back="Show Projects" data-front="My Projects"></span>
-        ) : (
-          <span>Hide Projects</span>
-        )}
-      </StyledButton>
-    </>
+    <StyledButton
+      active={isActive}
+      className={`${isActive ? "-active" : "-inactive"}`}
+      onClick={handleClick}
+    >
+      {!isActive ? (
+        <span data-back="Show Projects" data-front="My Projects"></span>
+      ) : (
+        <span>Hide Projects</span>
+      )}
+    </StyledButton>
   );
 };
 
